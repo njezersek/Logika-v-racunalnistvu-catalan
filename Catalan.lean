c6d4789ad8e04711efe3c87e4f8e62a5e269c622
Rok Filipovič <117859046+Roki2k@users.noreply.github.com>
2024-06-13 15:07:45 +0200

    Added T5

--- Catalan.lean
+++ Catalan.lean
@@ -2,5 +2,6 @@
 -- Import modules here that should be built as part of the library.
 -- import «Catalan».Basic
 import «Catalan».Tasks
-import «Catalan».T6_binomial_divisibility
 import «Catalan».T4_bijection_list_Plane_Tree_Plane_Tree
+import «Catalan».T5_rotating_isomorphism.lean
+import «Catalan».T6_binomial_divisibility

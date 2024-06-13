51e337e9ff038925342a8501ab8dde9ffc4213f7
Rok Filipovič <117859046+Roki2k@users.noreply.github.com>
2024-06-13 15:05:51 +0200

    Goals done, needs finishing touches

--- Catalan/T5_rotating_isomorphism.lean
+++ Catalan/T5_rotating_isomorphism.lean
@@ -1,166 +1,86 @@
 import Mathlib
-import Mathlib.Data.Set.Basic
-import Mathlib.Algebra.BigOperators.Fin
-import Mathlib.Algebra.Group.Defs
-import Mathlib.Data.Nat.Choose.Basic
-import Mathlib.Data.Nat.Factorial.Basic
-open BigOperators
-open Finset
-open Finset.antidiagonal
 
+-- plane trees definition
 inductive plane_tree : Type
 | node : List plane_tree → plane_tree
 deriving Repr
 
 open plane_tree
 
+-- full binary trees definition
 inductive full_binary_tree : Type
 | leaf : full_binary_tree
 | node : full_binary_tree → full_binary_tree → full_binary_tree
 deriving Repr
 
 open full_binary_tree
-
-def example1 : full_binary_tree :=
-  node (node leaf leaf) (node leaf leaf)
-
-def example2 : full_binary_tree :=
-  node (node leaf (node leaf leaf)) (node leaf leaf)
-
-def example_plane_tree_1 : plane_tree :=
-  node [node [], node [node []]]
-
-def example_plane_tree_2 : plane_tree :=
-  node [node [node []], node [node [node []], node []]]
-
-def blptapt_toFun : List plane_tree → plane_tree
+      
+def blptpt_toFun : List plane_tree → plane_tree -- T4 bijection list plane trees and plane trees (blptpt) toFun
 | N => .node N
 
-def blptapt_invFun : plane_tree → List plane_tree
+def blptpt_invFun : plane_tree → List plane_tree -- T4 bijection list plane trees and plane trees (blptpt) invFun
 | .node N => N
 
-def fun : plane_tree → List plane_tree
-.node N => N
-
-
-def bijection_list_plane_tree_and_plane_tree : List plane_tree ≃ plane_tree :=
-  {
-    toFun := blptapt_toFun --fun N => .node N -- Function that maps from List plane tree to plane tree
-    invFun := blptapt_invFun --fun (.node N) => N -- Function that maps from plane tree to List plane tree
-    left_inv := by -- Proof, one direction
-      intro x
-      cases x
-      case nil => rfl
-      case cons h t => rfl,
-    right_inv := by -- Proof, other direction
-      intro t
-      cases t
-      case node N => rfl
-      }
-
+-- function from plane trees to full binary trees
 def plane_tree_to_full_binary_tree : plane_tree → full_binary_tree
 | plane_tree.node [] => leaf
 | plane_tree.node (c :: cs) =>
     node (plane_tree_to_full_binary_tree c)
          (plane_tree_to_full_binary_tree (node cs))
 
--- def full_binary_tree_to_plane_tree : full_binary_tree → plane_tree
--- | leaf => plane_tree.node []
--- | full_binary_tree.node l r =>
---     match full_binary_tree_to_plane_tree r with
---       | plane_tree.node rs => node (full_binary_tree_to_plane_tree l :: rs)
-
+-- function from full binary trees to plane trees
 def full_binary_tree_to_plane_tree : full_binary_tree → plane_tree
 | full_binary_tree.leaf => plane_tree.node []
 | full_binary_tree.node l r => 
-  let l' := blptapt_invFun (full_binary_tree_to_plane_tree l)
-  let r' := blptapt_invFun (full_binary_tree_to_plane_tree r)
-  blptapt_toFun (plane_tree.node l' :: r')
-  
-#eval example1
-#eval full_binary_tree_to_plane_tree example1
-#eval plane_tree_to_full_binary_tree (full_binary_tree_to_plane_tree example1)
+  let l' := blptpt_invFun (full_binary_tree_to_plane_tree l)
+  let r' := blptpt_invFun (full_binary_tree_to_plane_tree r)
+  blptpt_toFun (plane_tree.node l' :: r')
 
-#eval example2
-#eval full_binary_tree_to_plane_tree example2
-#eval plane_tree_to_full_binary_tree (full_binary_tree_to_plane_tree example2)
+theorem left_inverse : ∀ (t : plane_tree), full_binary_tree_to_plane_tree (plane_tree_to_full_binary_tree t) = t := by
+  rintro ⟨⟨ ⟩ | ⟨t, l⟩⟩
 
+  -- case plane_tree.node []
+  { simp [plane_tree_to_full_binary_tree]
+    simp [full_binary_tree_to_plane_tree]
+    } 
 
-#eval example_plane_tree_1
-#eval plane_tree_to_full_binary_tree example_plane_tree_1
-#eval full_binary_tree_to_plane_tree (plane_tree_to_full_binary_tree example_plane_tree_1)
+  -- case plane_tree.node (t :: l)
+  { simp [plane_tree_to_full_binary_tree]
+    simp [full_binary_tree_to_plane_tree]
+    simp [blptpt_toFun]
 
-#eval example_plane_tree_2
-#eval plane_tree_to_full_binary_tree example_plane_tree_2
-#eval full_binary_tree_to_plane_tree (plane_tree_to_full_binary_tree example_plane_tree_2)
+    rw [left_inverse, left_inverse] -- recursive call on subproblems
+    match t with
+    | plane_tree.node l => 
+      simp [blptpt_invFun]
+  }
 
+lemma helper_right_inv : plane_tree.node (blptpt_invFun (full_binary_tree_to_plane_tree t)) = (full_binary_tree_to_plane_tree t) := by  
+  induction t
+  case leaf => rfl
+  case node l r _ _ =>
+    simp [full_binary_tree_to_plane_tree]
+    rw [blptpt_toFun]
+    rfl
 
-
-
-
-
-
+theorem right_inverse : ∀ (t : full_binary_tree), plane_tree_to_full_binary_tree (full_binary_tree_to_plane_tree t) = t := by
+  intro x
+  induction x
+  case leaf => 
+    simp [full_binary_tree_to_plane_tree]
+    simp [plane_tree_to_full_binary_tree]
+  case node l r ih_l ih_r =>
+    simp [full_binary_tree_to_plane_tree]
+    simp [blptpt_toFun]
+    simp [plane_tree_to_full_binary_tree]
+    rw [helper_right_inv, helper_right_inv]
+    rw [ih_l, ih_r]
+    simp  
 
 def rotating_isomorphism : plane_tree ≃ full_binary_tree :=
   { toFun := plane_tree_to_full_binary_tree,
     invFun := full_binary_tree_to_plane_tree,
-    left_inv := by
-      sorry
-      -- intro x
-      -- induction x
-      -- case node xs ih =>
-      --   cases xs
-      --   case nil => rfl
-      --   case cons h t =>
-      --     simp [plane_tree_to_full_binary_tree, full_binary_tree_to_plane_tree]
-      --     have h_inv := ih h
-      --     have t_inv := ih (node t)
-      --     rw [h_inv, t_inv],
-
-      -- intro x
-      -- induction x with
-      -- | node xs ih =>
-      --   cases xs
-      --   case nil => rfl
-      --   case cons h t =>
-      --     simp [plane_tree_to_full_binary_tree, full_binary_tree_to_plane_tree]
-      --     have h_inv := ih h
-      --     have t_inv := ih (node t)
-      --     rw [h_inv, t_inv],
-
-      -- intro x
-      -- have aux : ∀ t : plane_tree, full_binary_tree_to_plane_tree (plane_tree_to_full_binary_tree t) = t :=
-      --   fun t =>
-      --     match t with
-      --     | node [] => rfl
-      --     | node (h :: t') =>
-      --       have h_inv := aux h
-      --       have t_inv := aux (node t')
-      --       show full_binary_tree_to_plane_tree (plane_tree_to_full_binary_tree (node (h :: t'))) = node (h :: t') from
-      --         calc
-      --           full_binary_tree_to_plane_tree (plane_tree_to_full_binary_tree (node (h :: t')))
-      --           = full_binary_tree_to_plane_tree (full_binary_tree.node (plane_tree_to_full_binary_tree h) (plane_tree_to_full_binary_tree (node t'))) : rfl
-      --           ... = node (full_binary_tree_to_plane_tree (plane_tree_to_full_binary_tree h) :: match full_binary_tree_to_plane_tree (plane_tree_to_full_binary_tree (node t')) with | node rs => rs) : rfl
-      --           ... = node (full_binary_tree_to_plane_tree (plane_tree_to_full_binary_tree h) :: full_binary_tree_to_plane_tree (plane_tree_to_full_binary_tree (node t')) |>.cases_on (λ rs => rs)) : rfl
-      --           ... = node (h :: t') : by rw [h_inv, t_inv]
-      -- exact aux x,
-
-    right_inv := by
-      intro x
-      induction x
-      case leaf => 
-        simp [full_binary_tree_to_plane_tree]
-        simp [plane_tree_to_full_binary_tree]
-      case node l r ih_l ih_r =>
-        simp [full_binary_tree_to_plane_tree]
-        -- simp [blptapt_invFun]
-        simp [blptapt_toFun]
-        simp [plane_tree_to_full_binary_tree]
-        simp [blptapt_invFun]
-
-        
-        rw [ih_l, ih_r]
-        }
-
-
+    left_inv := left_inverse,
+    right_inv := right_inverse
+  }
 
